53e55ff7fa48ffde3239bfc0393baee1589f2af6
Rishabh Jain <jrishabh720@gmail.com>
2020-02-22 20:46:30 +0530

    Initial

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+//console.log("Starting");
+
+//setTimeout(()=>{
+//	console.log("0 Sec Later");
+//},2000)
+
+//setTimeout(()=>{
+//      console.log("2 Sec Later");
+//},1998)
+
+//console.log("Stopping");
