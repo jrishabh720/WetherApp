8a965ba64eab89071fefa3abe872326c9011f3aa
Rishabh Jain <jrishabh720@gmail.com>
2020-02-24 01:43:41 +0530

    Lang

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const key = "caa88eed7a5ab2702ce2ec4ff4794a42";
 const inCel = (temp) => (temp-273).toFixed(2);
-const url = `http://api.openweathermap.org/data/2.5/weather?lat=${29.953950}&lon=${76.810454}&appid=${key}`;
+const url = `http://api.openweathermap.org/data/2.5/weather?lat=${29.953950}&lon=${76.810454}&appid=${key}&lang=${'hi'}`;
 request({ url : url , json : true},function(error,response){
 	const data = response.body;
 //	console.log(data.weather[0].main + "is Todays Status and" + data.main.temp-273 + "is Todays Temperature");
