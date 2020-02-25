357d67b4416689f2a4ae6d4f14e62e9579fea1a1
Rishabh Jain <jrishabh720@gmail.com>
2020-02-25 23:05:09 +0530

    Mapbox Api

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ const url = `https://api.darksky.net/forecast/${"62d12f8cdd6b6f6868227ed367fe103
 request({ url : url , json : true},function(error,response){
 	console.log("It is currently " + response.body.currently.temperature + " degree out. There is a " + response.body.currently.precipProbability + "% chances of rain." );
 })
+const url1 = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${"pk.eyJ1IjoianJpc2hhYmg3MjAiLCJhIjoiY2s3MjVhaGdoMGQ4ZzNobXdpZXl4b3dmMSJ9.OgfzQJN79odC91Iw2eR4CQ"}&limit=1`;
+request({url : url1 , json : true},(error,response)=>{
+	console.log(response.body.features[0].center);	
+})
